a6aa9c8a67c37b1bbb6460ac0eb75ef0499b8aff
sntwandy <sntwandy@gmail.com>
2020-09-21 10:27:10 -0400

    Configuring the project and webpack

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const App = () => {
+  return <h1>I love you Megan E. :)</h1>;
+};
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  output: {
+    filename: "app.bundle[hash].js",
+  },
+  devServer: {
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [
+              ["@babel/plugin-transform-runtime", { regenerator: true }],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
